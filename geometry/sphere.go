a465791a1e41a8ec7a98899b0397e81785545593
James Thomas Pritchard <ProgrammerJames@users.noreply.github.com>
2018-02-24 21:45:29 +1100

    Update sphere.go

--- geometry/sphere.go
+++ geometry/sphere.go
@@ -5,5 +5,5 @@ import "github.com/programmerjames/go/vector"
 type Sphere struct {}
 
 func (s Sphere) Project(v vector.Vector) vector.Vector {
-	return vector.Multiply(v, -1.0/v.Magnitude())
+	return vector.Multiply(v, 1.0/v.Magnitude())
 }
